a960758f138e80ac83aa9a7f888be2fb41a6b3ad
Valentin BARBAZA <barbaza.valentin@gmail.com>
2023-05-18 13:44:35 +0100

    First commit, a ton of work has been made in testing (pytorch vs keras, etc)

---
+++ keras.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+# Load Packages
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+
+# from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Activation  # Generate 2 sets of X variables
+
+# LSTMs have unique 3-dimensional input requirements
+
+df = pd.read_csv("airline.csv")
+df2 = np.array(df["Number Passenger"])
+absi = np.array(df["Month"])
+X = df2[:-1]
+print(X.shape)
+exit()
+X = X.reshape((143, 1))
+Y = df2[1:]
+Y = Y.reshape((143, 1))
+
+model = Sequential()
+model.add(
+    LSTM(50, input_shape=(None, 1), return_sequences=False)
+)  # True = many to many
+# model.add(Dense(2,kernel_initializer='normal',activation='linear'))
+model.add(Dense(1, kernel_initializer="normal", activation="linear"))
+print(model.summary())
+model.compile(loss="mse", optimizer="adam", metrics=["accuracy"])
+model.fit(X, Y, epochs=2000, batch_size=1, validation_split=0.35, verbose=1)
+scores = model.evaluate(X, Y, verbose=1, batch_size=1)
+print("Accurracy: {}".format(scores[1]))
+
+predict = model.predict(X)
+print(predict)
+plt.plot(predict)
+
+# plt.plot(y, predict-y, 'C2')
+# plt.ylim(ymax = 3, ymin = -3)
+plt.show()
---
+++ pytorch.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import torch.optim as optim
+import torch.utils.data as data
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+import torch.nn as nn
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class AirModel(nn.Module):
+    def __init__(self, nbHidden):
+        super().__init__()
+        self.lstm = nn.LSTM(
+            input_size=1, hidden_size=nbHidden, num_layers=1, batch_first=True
+        )
+        self.linear = nn.Linear(nbHidden, 1)
+
+    def forward(self, x):
+        x, _ = self.lstm(x)
+        # x = x[:, -1, :]
+        x = self.linear(x)
+        return x
+
+
+class AirModel2(nn.Module):
+    def __init__(self, nbHidden):
+        super().__init__()
+        self.lstm = nn.LSTM(
+            input_size=1, hidden_size=nbHidden, num_layers=1, batch_first=True
+        )
+        self.linear1 = nn.Linear(nbHidden, 2)
+        self.linear2 = nn.Linear(2, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x, _ = self.lstm(x)
+        # x = x[:, -1, :]
+        x = self.linear1(x)
+        x = self.linear2(x)
+        # x = self.sigmoid(x)
+        return x
+
+
+def create_dataset(dataset, lookback):
+    X, y = [], []
+    for i in range(len(dataset) - lookback):
+        feature = dataset[i : i + lookback]
+        target = dataset[i + 1 : i + lookback + 1]
+        X.append(feature)
+        y.append(target)
+    return torch.tensor(X), torch.tensor(y)
+
+
+df = pd.read_csv("airline.csv")
+timeseries = df[["Passengers"]].values.astype("float32")
+
+# plt.plot(timeseries)
+# plt.show()
+
+# train-test split for time series
+train_size = int(len(timeseries) * 0.67)
+test_size = len(timeseries) - train_size
+train, test = timeseries[:train_size], timeseries[train_size:]
+
+
+lookback = 1
+X_train, y_train = create_dataset(train, lookback=lookback)
+X_test, y_test = create_dataset(test, lookback=lookback)
+print(X_train.shape, y_train.shape)
+print(X_test.shape, y_test.shape)
+
+model = AirModel2(4)
+# model = model.to(device)
+optimizer = optim.Adam(model.parameters())
+loss_fn = nn.MSELoss()
+loader = data.DataLoader(
+    data.TensorDataset(X_train, y_train), shuffle=True, batch_size=8
+)
+
+tmpW = list(model.linear1.parameters())
+
+n_epochs = 2000
+for epoch in range(n_epochs):
+    model.train()
+    for X_batch, y_batch in loader:
+        y_pred = model(X_batch)
+        loss = loss_fn(y_pred, y_batch)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    # Validation
+    if epoch % 100 != 0:
+        continue
+    model.eval()
+    with torch.no_grad():
+        y_pred = model(X_train)
+        train_rmse = torch.sqrt(loss_fn(y_pred, y_train))
+        y_pred = model(X_test)
+        test_rmse = torch.sqrt(loss_fn(y_pred, y_test))
+    print("Epoch %d: train RMSE %.4f, test RMSE %.4f" % (epoch, train_rmse, test_rmse))
+
+with torch.no_grad():
+    # shift train predictions for plotting
+    train_plot = np.ones_like(timeseries) * np.nan
+    y_pred = model(X_train)
+    y_pred = y_pred[:, -1, :]
+    train_plot[lookback:train_size] = model(X_train)[:, -1, :]
+    # shift test predictions for plotting
+    test_plot = np.ones_like(timeseries) * np.nan
+    test_plot[train_size + lookback : len(timeseries)] = model(X_test)[:, -1, :]
+# plot
+plt.plot(timeseries, c="b")
+plt.plot(train_plot, c="r")
+plt.plot(test_plot, c="g")
+plt.show()
+
+
+def getLSTMweights(lstm, nbInput, nbHidden):
+    nbGates = 4
+    weights = list(lstm.parameters())
+    out = [None] * nbGates
+    for i in range(4):
+        weights[i] = weights[i].detach().numpy()
+
+    weights[2] = weights[2] + weights[3]
+    weights.pop()
+
+    for i in range(nbGates):
+        for j in range(nbHidden):
+            out[i] = weights[0][i + j]
+            out[i].extend(weights[1][i + j])
+            out[i].append(weights[2][i + j])
+
+    return out
+
+
+def getLinearWeights(nn, nbOutput):
+    weights = list(nn.parameters())
+    out = []
+    for i in range(2):
+        weights[i] = weights[i].detach().numpy()
+    for i in range(nbOutput):
+        out.extend(weights[0][i])
+        out.append(weights[1][i])
+    return out
