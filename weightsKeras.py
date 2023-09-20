2e42142403d3293c53135043b692d022a74de547
Valentin BARBAZA <barbaza.valentin@gmail.com>
2023-09-20 16:35:44 +0200

    Got autoformated

--- weightsKeras.py
+++ weightsKeras.py
@@ -152,7 +152,7 @@ epochs = 300
 def create_dataset(ds, lookback=1):
     X, y = [], []
     for i in range(len(ds) - lookback):
-        feature = ds[i : i + lookback, 0]
+        feature = ds[i: i + lookback, 0]
         target = ds[i + lookback, 0]
         X.append(feature)
         y.append(target)
@@ -200,7 +200,7 @@ def main():
     # split into train and test sets
     train_size = int(len(ds) * 0.67)
     test_size = len(ds) - train_size
-    train, test = ds[0:train_size, :], ds[train_size : len(ds), :]
+    train, test = ds[0:train_size, :], ds[train_size: len(ds), :]
 
     # reshape into X=t and Y=t+1
     trainX, trainY = create_dataset(train, lookback)
@@ -247,8 +247,8 @@ def main():
 
     # Separate the train and test
     trainPredict, testPredict = (
-        predict[0 : train_size + 1, :],
-        predict[train_size : len(predict), :],
+        predict[0: train_size + 1, :],
+        predict[train_size: len(predict), :],
     )
 
     # calculate root mean squared error
@@ -261,10 +261,10 @@ def main():
 
     # shift train predictions for plotting
     trainPredictPlot = np.ones_like(ds) * np.nan
-    trainPredictPlot[lookback : len(trainPredict) + lookback, :] = trainPredict
+    trainPredictPlot[lookback: len(trainPredict) + lookback, :] = trainPredict
     # shift test predictions for plotting
     testPredictPlot = np.ones_like(ds) * np.nan
-    testPredictPlot[len(trainPredict) + 1 : len(ds), :] = testPredict
+    testPredictPlot[len(trainPredict) + 1: len(ds), :] = testPredict
 
     plt.plot(scaler.inverse_transform(ds), label="entire dataset")
     plt.plot(trainPredictPlot, label="trainPredict")
